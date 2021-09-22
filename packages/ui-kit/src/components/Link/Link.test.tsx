db4ca4e168220d097103a2d2e4e1387da23efb5f
Artem Balamutyuk <artyom.balamutyuk1@gmail.com>
2021-09-22 23:45:43 +0300

    Fix test create root error and add tests for link

--- packages/ui-kit/src/components/Link/Link.test.tsx
+++ packages/ui-kit/src/components/Link/Link.test.tsx
@@ -1,16 +1,34 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 
 import Link from './Link';
+import ThemeProvider, { themes, defaultThemeName } from '../../styles/theme/ThemeProvider';
+
+const theme = themes[defaultThemeName];
 
 describe('Link component', () => {
-  it('has a href attribute', () => {
-    const { getByRole } = render(
-      <Link href="https://storybook.js.org/tutorials/">
-        Link Text
-      </Link>,
+  beforeEach(() => {
+    render(
+      <ThemeProvider>
+        <Link href="https://storybook.js.org/tutorials/">
+          Link Text
+        </Link>
+      </ThemeProvider>,
     );
+  });
 
-    expect(getByRole('link')).toHaveAttribute('href', 'https://storybook.js.org/tutorials/');
-    expect(getByRole('link')).toHaveTextContent('Link Text');
+  it('has a href attribute', () => {
+    expect(screen.getByRole('link')).toHaveAttribute('href', 'https://storybook.js.org/tutorials/');
+  });
+
+  it('has given text', () => {
+    expect(screen.getByRole('link')).toHaveTextContent('Link Text');
+  });
+
+  it('has primary color and body1 variant by default', () => {
+    expect(screen.getByRole('link')).toHaveStyle({
+      color: theme.palette.primary.main,
+      fontSize: theme.typography.body1.fontSize,
+      fontWeight: theme.typography.body1.fontWeight,
+    });
   });
 });
